1d8a2368004e4c5b1e59110005952eb38cfd96c3
JJSerbot <47608741+JJSerbot@users.noreply.github.com>
2019-02-13 19:38:24 +0100

    Update Hoja 4 Ejercicio 2.py

--- Hoja 4 Ejercicio 2.py
+++ Hoja 4 Ejercicio 2.py
@@ -1,1 +1,17 @@
-ijuyb
+def es_divisible(n,d):
+  if n % d == 0:
+    return True
+  else:
+    return False
+
+def es_primo(n):
+  comp = 0
+  for i in range(2,n+1):
+    if es_divisible(n,i):
+      comp=0
+    else:
+      comp = 1
+    if comp == 1:
+      return True
+    else:
+      return False
